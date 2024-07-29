b16f37c4911e7aee01c39300f7091825310968b6
GijsLappenschaar <glappenschaar@gmail.com>
2024-07-29 09:34:50 +0200

    Add files via upload

---
+++ VijfStatements.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VijfStatements{
+    public static void main(String[] args) {
+    	System.out.println("1");
+    	System.out.println("2");
+    	System.out.println("3");
+    	System.out.println("4");
+    	System.out.println("5");
+
+    }
+  
+
+
+}
