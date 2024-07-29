26f1a0dd7a311aff1ca058750754ad443b3cfb23
GijsLappenschaar <glappenschaar@gmail.com>
2024-07-29 09:30:18 +0200

    Add files via upload

---
+++ IetsAndersZeggen.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class IetsAndersZeggen{
+    public static void main(String[] args) {
+    	System.out.println("Dit is iets anders dan hallo wereld!");
+
+
+
+    }
+
+
+
+
+}
