e8b69e77b8c30ffb3228467118fdbda0ebeae397
GijsLappenschaar <glappenschaar@gmail.com>
2024-07-30 08:25:06 +0200

    Add files via upload

---
+++ Brood.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Brood{
+    public static void main(String[] args) {
+    	Scanner brood = new Scanner(System.in);
+    	System.out.println("Het brood is klaar");
+    	System.out.println("Voor wie is het brood: ");
+    	String broodpersoon = brood.nextLine();
+    	System.out.println(broodpersoon + " krijgt zijn brood!");
+
+
+
+    }
+
+
+
+}
