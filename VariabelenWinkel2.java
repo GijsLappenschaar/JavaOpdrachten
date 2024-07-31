6eed1dfe99abb798e77aa0c99978ad911630dbd3
GijsLappenschaar <glappenschaar@gmail.com>
2024-07-31 06:06:48 +0200

    Add files via upload

---
+++ VariabelenWinkel2.java
@@ -0,0 +1,28 @@
+class VariabelenWinkel2{
+    public static void main(String[] args) {
+    	byte a = 20;
+    	short b = 19;
+    	int c = 2763;
+    	long d = 28978;
+    	float e = 8.22f;
+    	double f = 9283.20893;
+    	boolean g = true;
+    	char h = 'g'; 
+    	a = 49;
+    	b = 90;
+    	c = 324;
+    	d = 3234;
+    	e = 12.4f;
+    	f = 23132.212;
+    	g = false;
+    	h = 't';
+    	System.out.println(a);
+    	System.out.println(b);
+    	System.out.println(c);
+    	System.out.println(d);
+    	System.out.println(e);
+    	System.out.println(f);
+    	System.out.println(g);
+    	System.out.println(h);
+    }
+}
