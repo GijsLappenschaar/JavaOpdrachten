ed5e77fd85595c37d125bcc47872b75e9433ff1d
GijsLappenschaar <glappenschaar@gmail.com>
2024-07-31 06:20:16 +0200

    Add files via upload

---
+++ Driehoek.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Driehoek{
+	public static void main(String[] args) {
+		Scanner input1 = new Scanner(System.in); 
+		System.out.print("Vul de hoogte in: ");
+		int hoogte = input1.nextInt();
+		System.out.print("Vul de breedte in: ");
+		int breedte = input1.nextInt();
+
+		System.out.println(hoogte * breedte / 2);
+
+
+
+
+
+	}
+
+
+
+
+
+
+}
