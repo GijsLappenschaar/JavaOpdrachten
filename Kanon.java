0c68e399d51581114ced0a6daccd1eb5e708c98b
GijsLappenschaar <glappenschaar@gmail.com>
2024-08-01 17:24:36 +0200

    Add files via upload

---
+++ Kanon.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class Kanon {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 100; i++){
+			if ((i % 3) != 0 && (i % 5) != 0){
+				System.out.println(i + ": normaal" );
+			}
+			else if ((i%3) == 0 && (i % 5 )!=0){
+				System.out.println(i + ": Vuur");}
+			else if ((i % 3) != 0 && (i % 5 )==0){
+				System.out.println(i + ": Elektrisch");
+			}
+			else if ((i% 3)==0&&(i%5)==0){
+				System.out.println(i+": Vuur en Elektrisch");
+			}
+
+
+
+			
+			     
+
+
+
+
+
+		}
+			}
+		
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
